e9a929a43ce90213f1f5b84d53d0b8fdbe8a2564
Saksham Sonkar <ss9838157875@gmail.com>
2022-11-23 21:31:19 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -9,7 +9,7 @@ class Translation( object ):
                               "rename or video conversions</code> "
     SAVED_RECVD_DOC_FILE = "<b>✅ Media Downloaded Successfully</b>"
     ACCESS_DENIED_TEXT = "You're not Authorized to use this Bot."
-    START_TEXT = "Hey.. <b>{}</b>\n\n<b>I'm -</b> {}\n\n<b>Things I Can Do:</b>\n" \
+    START_TEXT = "Hey.. <b>{}</b>\n\n<b>I'm</b> {}\n\n<b>Things I Can Do:</b>\n" \
                  "➟ Rename File \n➟ Convert to Video/File \n➟ Custom Thumbnail \n➟ Send Media to 5 Different Channels/Groups \n\n" \
                  "<i>Send filename + extension (Eg: Pokemon.mkv) as a reply to any telegram media, and follow " \
                  "Bot's instructions.</i>\n\n" \
@@ -30,7 +30,7 @@ class Translation( object ):
     EXTENSIONS = ['.mkv', '.mp4', '.avi', '.webm']
     NO_SPAM_MSG = "⚠️ <b>Don't Spam Here</b>\n<code>Read the welcome message for better use of this bot !</code>"
     MAKE_A_COPY_TEXT = "<b>📚 You Need a Copy ?</b>"
-    CAPTION_TEXT = "<b>📁 Filename : {}</b>\n\nJoin <a href="https://t.me/anime_mine_library">Anime Mine</a>"
+    CAPTION_TEXT = "<b>📁 Filename : {}</b>"
     SUCCESSFUL_SEND = "<code>Forwarded Successfully to:</code>\n<b>{}</b>"
     FORWARD_ERROR = "<b>⚠️ Attention :</b>\n<code>Make Sure That I am Admin in Your Channel or Provided Channel " \
                     "ID is Correct.</code>"
